0963da0a9a1002cd6ab69f691dd12de2bb16546a
Olha Savva <106154412+OlhaSavva@users.noreply.github.com>
2023-01-13 01:34:09 +0200

    New

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+function getGalleryElement() {
+  return document.querySelector(".gallery");
+}
+function createImageElement({ preview, original, description }) {
+  return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+</a>
+</div>
+    `;
+}
+function onGalleryImageClick(container) {
+  return container.addEventListener("click", handelGalleryImageClick);
+}
+function handelGalleryImageClick(evt) {
+  const isImageElement = evt.target.classList.contains("gallery__image");
+  if (!isImageElement) {
+    return;
+  }
+  console.log(evt.target);
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
 
-console.log(galleryItems);
+  instance.show();
+}
+
+function rander() {
+  console.log(galleryItems);
+  const container = getGalleryElement();
+  const elementsToRander = galleryItems.map(createImageElement).join("");
+  container.insertAdjacentHTML("beforeend", elementsToRander);
+  onGalleryImageClick(container);
+}
+rander();
