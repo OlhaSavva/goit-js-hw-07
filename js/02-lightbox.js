a8645afae1cb922d08d79e980f31537a7587b09f
Olha Savva <106154412+OlhaSavva@users.noreply.github.com>
2023-01-20 00:13:35 +0200

    new

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-function getGalleryElement() {
-  return document.querySelector(".gallery");
-}
+const gallery = document.querySelector(".gallery");
+
 function createImageElement({ preview, original, description }) {
   return `
     <a class="gallery__item" href="${original}" onclick="return false">
@@ -10,26 +9,10 @@ function createImageElement({ preview, original, description }) {
 </a>
  `;
 }
-function onGalleryImageClick(container) {
-  return container.addEventListener("click", handelGalleryImageClick);
-}
-function handelGalleryImageClick(evt) {
-  const isImageElement = evt.target.classList.contains("gallery__image");
-  if (!isImageElement) {
-    return;
-  }
+const elementsToRander = galleryItems.map(createImageElement).join("");
+gallery.insertAdjacentHTML("beforeend", elementsToRander);
 
-  let gallery = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-  gallery.open();
-}
-
-function rander() {
-  const container = getGalleryElement();
-  const elementsToRander = galleryItems.map(createImageElement).join("");
-  container.insertAdjacentHTML("beforeend", elementsToRander);
-  onGalleryImageClick(container);
-}
-rander();
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
